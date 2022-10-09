5a93116c8a52c3a865bc487bc20cf6fb67017ea5
Andrii Beznosov <andrey301288@gmail.com>
2022-10-09 22:44:38 +0300

    +

--- src/components/contans/Theme.js
+++ src/components/contans/Theme.js
@@ -1,13 +1,13 @@
-export const theme = {
-  colors: {
-    white: '#fff',
-    black: '#000',
-    green: '#4caf50',
-    red: '#f44336',
-    blue: '#2196f3',
-    primaryText: '#212121',
-    secondaryText: '#808080',
-    backgroundColor: '#f0f0f1',
-  },
-  space: value => `${4 * value}px`,
-};
+// export const theme = {
+//   colors: {
+//     white: '#fff',
+//     black: '#000',
+//     green: '#4caf50',
+//     red: '#f44336',
+//     blue: '#2196f3',
+//     primaryText: '#212121',
+//     secondaryText: '#808080',
+//     backgroundColor: '#f0f0f1',
+//   },
+//   space: value => `${4 * value}px`,
+// };
